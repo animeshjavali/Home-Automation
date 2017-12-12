268e88b29f7ba044d3011657483a6b61890d4b64
Animesh Javali <33710255+animeshjavali@users.noreply.github.com>
2017-12-12 07:03:48 -0800

    Add files via upload

---
+++ HOME_AUTOMATION_IOT.ino
@@ -0,0 +1,20 @@
+int data;
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()>0)
+  {
+  data=Serial.parseInt();
+  if(data==1)
+  digitalWrite(13,HIGH);
+  else if(data==2)
+  digitalWrite(13,LOW);
+
+  }
+  
+  }// put your main code here, to run repeatedly:
+
+
