cb08b5084058a35248c946bab08c4968b2b5d1f7
Animesh Javali <33710255+animeshjavali@users.noreply.github.com>
2018-03-04 12:23:31 +0530

    Update ESP8266_Blynk.ino

--- ESP8266_Blynk.ino
+++ ESP8266_Blynk.ino
@@ -1,40 +1,35 @@
-
 #include <SPI.h>
-
 #include <BlynkSimpleEsp8266.h>
 
- int j ;
-char *ssid="Ani";
-char *pass="ibringit";
-char *auth = "f788d57d563942378dc4401cfe488d37";
+int j ;
+char *ssid="WiFi_NETWORK_NAME";
+char *pass="WiFi_PASSWORD";
+char *auth = "f788d57d563942378dc4401cfe488d37";//Unique authentication token given by blynk.
 
 void setup() {
 Serial.begin(115200);
 delay(100);
-pinMode(D1,OUTPUT);
+
 pinMode(D2,OUTPUT);
 pinMode(D3,OUTPUT);
-pinMode(D4,OUTPUT);
+pinMode(D4,OUTPUT);// Set outpins on NodeMCU.
 pinMode(D5,OUTPUT);
 pinMode(D6,OUTPUT);
-pinMode(D7,OUTPUT);
-pinMode(D8,OUTPUT);
-pinMode(D9,OUTPUT);
-pinMode(D10,OUTPUT);
-Blynk.begin(auth, ssid, pass);
+
+Blynk.begin(auth, ssid, pass);// start blynk server.
 delay(100);
 }
 
  BLYNK_WRITE(V1)
 {
- j = param.asInt();
+ j = param.asInt();//Parse data from Blynk android app to select an LED.
  Serial.print("LED ");
 Serial.print(j);
 Serial.println(" is selected");
 }
  BLYNK_WRITE(V0)
 {
-int i = param.asInt();
+int i = param.asInt();//Parse data from Blynk android app to control an LED.
 
 if(j==1){ 
 if(i == 1){
@@ -107,20 +102,7 @@ Serial.println(" is turned OFF");
 }
 }
 }
-/* if(j==5){ 
-if(i == 1){
-digitalWrite(D6, HIGH);
-Serial.print("LED ");
-Serial.print(j);
-Serial.println(" is turned ON");
-}
-else if(i == 0){
-digitalWrite(D6, LOW);
-Serial.print("LED ");
-Serial.print(j);
-Serial.println(" is turned OFF");
-}
-}*/
+
 void loop() {
- Blynk.run();
+ Blynk.run();// Runs in an infinite loop.
 }
