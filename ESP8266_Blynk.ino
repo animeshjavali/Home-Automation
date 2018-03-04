470fb50ae764f2e6c6beb2771dea3b103d355f49
Animesh Javali <33710255+animeshjavali@users.noreply.github.com>
2018-03-04 12:18:56 +0530

    Add files via upload

---
+++ ESP8266_Blynk.ino
@@ -0,0 +1,126 @@
+
+#include <SPI.h>
+
+#include <BlynkSimpleEsp8266.h>
+
+ int j ;
+char *ssid="Ani";
+char *pass="ibringit";
+char *auth = "f788d57d563942378dc4401cfe488d37";
+
+void setup() {
+Serial.begin(115200);
+delay(100);
+pinMode(D1,OUTPUT);
+pinMode(D2,OUTPUT);
+pinMode(D3,OUTPUT);
+pinMode(D4,OUTPUT);
+pinMode(D5,OUTPUT);
+pinMode(D6,OUTPUT);
+pinMode(D7,OUTPUT);
+pinMode(D8,OUTPUT);
+pinMode(D9,OUTPUT);
+pinMode(D10,OUTPUT);
+Blynk.begin(auth, ssid, pass);
+delay(100);
+}
+
+ BLYNK_WRITE(V1)
+{
+ j = param.asInt();
+ Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is selected");
+}
+ BLYNK_WRITE(V0)
+{
+int i = param.asInt();
+
+if(j==1){ 
+if(i == 1){
+digitalWrite(D2, HIGH);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned ON");
+}
+else if(i == 0){
+digitalWrite(D2, LOW);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned OFF");
+}
+}
+if(j==2){ 
+if(i == 1){
+digitalWrite(D3, HIGH);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned ON");
+}
+else if(i == 0){
+digitalWrite(D3, LOW);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned OFF");
+}
+}
+if(j==3){ 
+if(i == 1){
+digitalWrite(D4, HIGH);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned ON");
+}
+else if(i == 0){
+digitalWrite(D4, LOW);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned OFF");
+}
+}
+if(j==4){ 
+if(i == 1){
+digitalWrite(D5, HIGH);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned ON");
+}
+else if(i == 0){
+digitalWrite(D5, LOW);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned OFF");
+}
+}
+if(j==5){ 
+if(i == 1){
+digitalWrite(D6, HIGH);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned ON");
+}
+else if(i == 0){
+digitalWrite(D6, LOW);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned OFF");
+}
+}
+}
+/* if(j==5){ 
+if(i == 1){
+digitalWrite(D6, HIGH);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned ON");
+}
+else if(i == 0){
+digitalWrite(D6, LOW);
+Serial.print("LED ");
+Serial.print(j);
+Serial.println(" is turned OFF");
+}
+}*/
+void loop() {
+ Blynk.run();
+}
